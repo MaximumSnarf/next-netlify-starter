b7edb98bec73ba2fc6fa9cde7aeb5e6a4db557ad
MaximumSnarf <118850495+MaximumSnarf@users.noreply.github.com>
2022-11-22 13:19:52 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+    <model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="shared-assets/models/NeilArmstrong.glb" ar environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp" shadow-intensity="1" camera-controls touch-action="pan-y"></model-viewer>
       </main>
 
       <Footer />
