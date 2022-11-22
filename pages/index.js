156c31418d23ca26b0f6d309cc34b2ea7a4e378b
MaximumSnarf <118850495+MaximumSnarf@users.noreply.github.com>
2022-11-22 12:58:30 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,18 +14,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-      <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-          <model-viewer src="Rock_1.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1">
-    <div class="progress-bar hide" slot="progress-bar">
-        <div class="update-bar"></div>
-    </div>
-    <button slot="ar-button" id="ar-button">
-        View in your space
-    </button>
-    <div id="ar-prompt">
-        <img src="https://modelviewer.dev/shared-assets/icons/hand.png">
-    </div>
-</model-viewer>
+  
         </p>
       </main>
 
