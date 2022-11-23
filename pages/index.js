36d9f1802b4e9143eb8c1c2ded0fcf2617c2ed2c
MaximumSnarf <118850495+MaximumSnarf@users.noreply.github.com>
2022-11-23 10:09:55 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+        <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
         <title>Next.js Starter!</title>
-          <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+         
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
